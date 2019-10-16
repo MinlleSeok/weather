84e6ccbea68be980795eb0b76cbd1d1d85dd8c78
Your Name <you@example.com>
2019-10-16 21:39:56 +0900

    191016 get Weather api

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Alert} from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
 const API_KEY = '981de81e717445464457a8a94529f9bb';
 
@@ -11,13 +12,20 @@ export default class App extends React.Component {
     isLoading: true
   }
 
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+  }
+
   getLocation = async () => {
     try {
       // throw Error();
       await Location.requestPermissionsAsync();
       const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
       // Send to API and get Weather
-      
+      this.getWeather(latitude, longitude);
       //console.log(coords);
       this.setState({ isLoading : false });
     } catch (error) {
